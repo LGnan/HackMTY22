8c8ea262ac183e685f9a4bc6006343679c458700
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2022-09-24 20:00:46 -0500

    Si

---
+++ Food4All/autenticacion/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import View
+# Create your views here.
+
+class VRegistro(View):
+    def get(self,request):
+        pass
+    def post(self,request):
+        pass
